4eb370651037127dfe668f796842e67e1aac8f70
Anna Moignard <amoignard.coder@gmail.com>
2020-11-06 16:38:35 +1100

    added aws storage for uploading logos to new brand

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -12,9 +12,12 @@ class BrandsController < ApplicationController
   end
 
   def create
-    brand = current_user.brand.new(name: params[:brand][:name], price_range: params[:brand][:price_range], speciality: params[:brand][:speciality])
-    brand.save 
-    redirect_to brand_path #so they can view the brand that they created
+    brand = Brand.new(name: params[:brand][:name], price_range: params[:brand][:price_range], speciality: params[:brand][:speciality])
+    brand.save
+    brand.picture.attach(params[:brand][:picture])
+    # assiging a new instance of a brand to the has_one relationship
+    current_user.brand = brand #setting up relationship between brand and the user, and giving the brand a user id
+    redirect_to brand_path(brand.id) #so they can view the brand that they created
   end
 
   # def edit
