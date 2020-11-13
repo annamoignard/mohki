21baaae60552b90623f9f8e25bcbff25961e4ba4
Anna Moignard <amoignard.coder@gmail.com>
2020-11-13 11:21:51 +1100

    configuring navbar for different user views

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -26,12 +26,6 @@ class LikesController < ApplicationController
     @like = Like.where(user_id: current_user.id, listing_id: params[:id]).first 
   end
 
-
 end
 
 
-# need to add route? done this 
-# need a view page for user to view liked products? done 
-# associations are wrong - join table ????  done 
-# is it product or listing? I guess listing coz i am 'liking' the listing 
-# only someone logged in can like 
